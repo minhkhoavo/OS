c8578340f4e33183fa8c69f94240670f05a759bc
minhkhoavo <v.minhkhoa123456@gmail.com>
2024-01-22 15:24:44 +0700

    update ex12

--- Excercise/2. Shell Program/13.sh
+++ Excercise/2. Shell Program/13.sh
@@ -1,10 +1,17 @@
-file_path="$1"
-
-if [ ! -e "$file_path" ]; then
-    echo "File khong ton tai."
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <file_path>"
     exit 1
 fi
 
-group_permissions=$(stat -c "%G %a" "$file_path")
+file_path="$1"
+
+# Kiểm tra xem tập tin có tồn tại hay không
+if [ ! -e "$file_path" ]; then
+    echo "File does not exist."
+    exit 1
+fi
+
+group_permissions=$(stat -c "%A" "$file_path" | cut -c6-8)
 
 echo "$file_path: $group_permissions"
+
